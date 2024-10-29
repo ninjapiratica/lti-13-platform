891b4a7c9967b0513fd4234df4fae0b490bd67cd
Chase Hanson <ninja.piratica@gmail.com>
2024-10-29 16:59:47 -0700

    convert records from positional to properties

--- NP.Lti13Platform.DeepLinking/DeepLinkSettingsOverride.cs
+++ NP.Lti13Platform.DeepLinking/DeepLinkSettingsOverride.cs
@@ -1,4 +1,16 @@
 ﻿namespace NP.Lti13Platform.DeepLinking
 {
-    public record DeepLinkSettingsOverride(string? DeepLinkReturnUrl, IEnumerable<string>? AcceptTypes, IEnumerable<string>? AcceptPresentationDocumentTargets, IEnumerable<string>? AcceptMediaTypes, bool? AcceptMultiple, bool? AcceptLineItem, bool? AutoCreate, string? Title, string? Text, string? Data);
+    public record DeepLinkSettingsOverride
+    {
+        public string? DeepLinkReturnUrl { get; set; }
+        public IEnumerable<string>? AcceptTypes { get; set; }
+        public IEnumerable<string>? AcceptPresentationDocumentTargets { get; set; }
+        public IEnumerable<string>? AcceptMediaTypes { get; set; }
+        public bool? AcceptMultiple { get; set; }
+        public bool? AcceptLineItem { get; set; }
+        public bool? AutoCreate { get; set; }
+        public string? Title { get; set; }
+        public string? Text { get; set; }
+        public string? Data { get; set; }
+    }
 }
